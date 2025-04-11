8056b239c6c8d90538b049565c0534837ef9c6c9
saireddykummetha <kummethasaikumarreddy@gmail.com>
2025-04-11 15:08:25 +0530

    Update Frame1.js

--- frontend/src/Frame1/Frame1.js
+++ frontend/src/Frame1/Frame1.js
@@ -2,45 +2,58 @@ import React from 'react'
 import "./Frame1.css";
 import { IoIosSearch } from "react-icons/io";
 import { CiLocationOn } from "react-icons/ci";
-import { RiArrowDropDownLine } from "react-icons/ri";
 import { RxPerson } from "react-icons/rx";
 import { BsPersonPlus } from "react-icons/bs";
 import { PiBuildingsThin } from "react-icons/pi";
 import { GiMoneyStack } from "react-icons/gi";
 import amazon from "../images/amazon.png";
 import tesla from "../images/tesla.png";
-import Swiggy from "../images/Swiggy.png";
+
+import { useState } from 'react'
 
 import Navbar from '../Navbar';
 
 
 const Frame1 = () => {
+  const [price,setprice]=useState('80');
+   const [dropdownvisible,setdropdownvisible]=useState(false);
+   const[selected,setselected]=useState(null);
+   const[itemlist,setitemlist]=useState([
+    {
+      name:'Internship',
+      value:"internship"
+    },
+    {
+      name:'FullTime',
+      value:"fulltime"
+    },
+    {
+      name:'PartTime',
+      value:"partTime"
+    },
+    {
+      name:'Contract',
+      value:"contract"
+    },
+   ])
+   
+   const[locationvisible,setlocationvisible]=useState(false);
+   const[locationselected,setlocationselected]=useState(null);
+   const[list,setlist]=useState([
+    {
+       name:'Onsite',
+      value:"onsite"
+    },
+    {
+      name:'Offsite',
+     value:"offsite"
+   }
+   ])
   return (
-    <div className="frame-1">
+    <div className="frame-1" >
        
        
-        <div className="frame-2">
-             <div className="vector"> <IoIosSearch /></div>  
-            <div className="search-bar">
-            <input className="search-bar1" type='text' placeholder='Search By Job Title, Role'/>
-            </div>
-            <div className="line">|</div>
-            <div className="vector1"> <CiLocationOn /></div>
-            <div className="location">Preferred Location </div>
-            <div className="vector2">< RiArrowDropDownLine/></div>
-            <div className="line1">|</div>
-            <div className="jobtype">Job type </div>
-            <div className="vector3"> <RxPerson /></div>
-            <div className="vector4">< RiArrowDropDownLine/></div>
-            <div className="line2">|</div>
-            <div className="salary-per-month">Salary Per Month </div>
-            <div className="_50-k-80-k">₹50k - ₹80k </div>
-            <div className="barline">_______________</div>
-            <div className="barline1">__________</div>
-            <div className="dot">. </div>
-            <div className="dot1">. </div>
-        </div>
-        
+       
         <div className="table">
           <div className='table1'>
             
@@ -174,11 +187,8 @@ const Frame1 = () => {
           <div className="frameapply">
             <div className="apply-now">Apply Now </div>
           </div>
-            </div>
-               
-            
+            </div>     
         </div>
-
 
         <div className='frame3'>
             <div className='table6'>
@@ -318,9 +328,80 @@ const Frame1 = () => {
             </div>
            </div>
            
+
+           <div className="frame-2">
+             <div className="vector"> <IoIosSearch /></div>  
+            <div className="search-bar">
+            <input className="search-bar1" type='text' placeholder='Search By Job Title, Role'/>
+            </div>
+            <div className="line">|</div>
+            <div className="vector1"> <CiLocationOn /></div>
+           
+            <div className='location-dropdown'>
+              <div className='location-selected'
+                onClick={e=>{ setlocationvisible(!locationvisible);
+                }}>
+              {locationselected != null ? list[locationselected].name:"Preferred Location"}
+              </div>
+              {locationvisible ? ( <div className="item-holder">
+               {
+                list.map((item,index)=>(
+                  <div key={item.value} className='dropdown-items' onClick={e=>{
+                    setlocationselected(index);
+                    setlocationvisible(false);
+                  }}>
+                    {item.name}
+                  </div>
+                ))
+               }
+              </div>):<></>}
+            </div>
+            <div className="line1">|</div>
+           
+            <div className="custom-dropdown" >
+              <div className="dropdown-selected" 
+              onClick={e=>{ setdropdownvisible(!dropdownvisible);
+              }}>
+                {selected != null ? itemlist[selected].name:"Jobtype"}
+              </div>
+             {dropdownvisible ? ( <div className="items-holder">
+               {
+                itemlist.map((item,index)=>(
+                  <div key={item.value} className='dropdown-item' onClick={e=>{
+                    setselected(index);
+                    setdropdownvisible(false);
+                  }}>
+                    {item.name}
+                  </div>
+                ))
+               }
+              </div>):<></>}
+            </div>
+           
+             <div className="frame6-1" >   
+              
+            </div>
+            <div className="vector3"> <RxPerson /></div>
+            
+            <div className="line2">|</div>
+            <div className="salary-per-month">Salary Per Month </div>
+            <div className="_50-k-80-k">₹50k - ₹{price}k </div>
+            <div className="barline">
+            <div className="barline1" style={{'width':(price/100*100) +'%'}}></div>
+            
+            </div>
+            <div className="slider"> 
+           
+               <input onChange={(e)=>setprice(e.target.value >=0 
+              && e.target.value <=100 ? e.target.value:"100")}value={price}
+               type='range' className="range-min" min="0" max="100" />             
+              <input  value="0"type='range' className="range-max" min="50" max="100"/>             
+            </div>
+        </div>
            <Navbar/>
         
     </div>
+    
   )
 }
 
