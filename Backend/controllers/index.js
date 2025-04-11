130617b69cfd3a14a0f279fa3b1ba1a348093d66
saireddykummetha <kummethasaikumarreddy@gmail.com>
2025-04-11 15:18:43 +0530

    Update index.js

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,24 +1,2 @@
 
 
-// import details from '../modal';
-
-// async function uploaddetails(req,res){
-//   try{
-//   const upload=details(req.body)
-//   const save=await upload.save()
-//   res.status(200).json({
-//     message:"upload succesfully",
-//     error:false,
-//     success:true,
-//     data:save
-//   })
-//   }catch(err){
-//   res.status(400).json({
-//     message:err.message || err,
-//     error:true,
-//     success:false
-//   })
-//   }
-// }
-
-// export default uploaddetails;
